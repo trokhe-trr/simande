239b1db7ffa6d631d455f109efd437be6605aace
trokhe-trr <thomasrinconreis@gmail.com>
2017-09-14 00:21:42 -0300

    trab.html

--- trab.js
+++ trab.js
@@ -1,1 +1,37 @@
+function random(n) {
+			console.log("aquip");
+			var mult = 11834;
+      var x = Math.cos(n) * mult;
+      return x - Math.floor(x);
+};
 
+
+var getVector = function(start,end,qt){
+  var today = new Date();
+  var n = today.getTime();
+
+	var seed = n;  
+  
+  var vet = [];
+  console.log("aqui3");
+  
+  for(var i=0; i<qt; i++){
+    var r = random(seed++)*(end-start)+start
+    console.log("aquix");
+    vet.push(r);
+  }
+  
+  console.log("aqui4");
+  	  
+  return vet;
+};
+
+function calculate() {
+	var start = document.getElementById('interval_min').value;
+    var end = document.getElementById('interval_max').value;
+    var qt = document.getElementById('qt').value;
+    var result = getVector(start,end,qt);
+
+    document.getElementById('result').innerHTML = "["+result+"]";
+   
+}
